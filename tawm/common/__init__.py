aac2a162520866e220d828b7b1eb60eecc9364b0
anh-nn01 <anhnhuvn2001@gmail.com>
2025-06-03 13:30:48 -0400

    code cleaning

--- tawm/common/__init__.py
+++ tawm/common/__init__.py
@@ -24,62 +24,19 @@ MODEL_SIZE = { # parameters (M)
 }
 
 TASK_SET = {
-    'dmcontrol': [
-        # 19 original dmcontrol tasks
-		'walker-stand', 'walker-walk', 'walker-run', 'cheetah-run', 'reacher-easy',
-	    'reacher-hard', 'acrobot-swingup', 'pendulum-swingup', 'cartpole-balance', 'cartpole-balance-sparse',
-		'cartpole-swingup', 'cartpole-swingup-sparse', 'cup-catch', 'finger-spin', 'finger-turn-easy',
-		'finger-turn-hard', 'fish-swim', 'hopper-stand', 'hopper-hop', 'quadruped-walk', 'quadruped-run',
-		# 11 custom dmcontrol tasks
-		'walker-walk-backwards', 'walker-run-backwards', 'cheetah-run-backwards', 'cheetah-run-front', 'cheetah-run', 
-        'cheetah-run-back', 'cheetah-jump', 'hopper-hop-backwards', 'reacher-three-easy', 'reacher-three-hard', 'cup-spin',
-		'pendulum-spin',
-	],
-   'highway': [
-       'highway-v0', 'merge-v0', 'roundabout-v0', 'parking-v0', 'intersection-v0', 'racetrack-v0',
-	], 
 	'mt9': [
-		# 4 original dmcontrol tasks
-		'walker-walk', 'cheetah-run', 'reacher-easy', 'cup-catch',
-		# 5 meta-world tasks
-		'mw-basketball','mw-coffee-button', 'mw-faucet-open', 'mw-soccer', 'mw-window-close',
+		# 9 meta-world tasks
+		'mw-assembly', 'mw-basketball', 'mw-box-open', 
+        'mw-coffee-button', 'mw-faucet-open', 'mw-handle-pull',
+        'mw-lever-pull', 'mw-soccer', 'mw-window-close',
 	],
 	'mt10': [
-		# 5 original dmcontrol tasks
-		'walker-walk', 'cheetah-run', 'reacher-easy', 'acrobot-swingup', 'cup-catch',
-		# 5 meta-world tasks
-		'mw-basketball','mw-coffee-button', 'mw-faucet-open', 'mw-soccer', 'mw-window-close',
+		# 10 meta-world tasks
+		'mw-assembly', 'mw-basketball', 'mw-box-open', 
+        'mw-coffee-button', 'mw-faucet-open', 'mw-handle-pull',
+        'mw-lever-pull', 'mw-stick-pull', 'mw-soccer', 'mw-window-close',
 	],
-	'mt25': [
-		# 10 original dmcontrol tasks
-		'walker-walk', 'cheetah-run', 'reacher-easy', 'acrobot-swingup', 'cup-catch',
-		'walker-stand', 'walker-run', 'reacher-hard', 'pendulum-swingup', 'finger-spin',
-		# 15 meta-world tasks
-		'mw-basketball', 'mw-soccer', 'mw-sweep', 'mw-coffee-button', 'mw-coffee-pull',
-		'mw-assembly', 'mw-disassemble', 'mw-hammer', 'mw-door-open', 'mw-door-close', 
-		'mw-window-open', 'mw-window-close', 'mw-sweep-into', 'mw-push', 'mw-push-wall',
-	],
-	'mt30': [
-		# 19 original dmcontrol tasks
-		'walker-stand', 'walker-walk', 'walker-run', 'cheetah-run', 'reacher-easy',
-	    'reacher-hard', 'acrobot-swingup', 'pendulum-swingup', 'cartpole-balance', 'cartpole-balance-sparse',
-		'cartpole-swingup', 'cartpole-swingup-sparse', 'cup-catch', 'finger-spin', 'finger-turn-easy',
-		'finger-turn-hard', 'fish-swim', 'hopper-stand', 'hopper-hop',
-		# 11 custom dmcontrol tasks
-		'walker-walk-backwards', 'walker-run-backwards', 'cheetah-run-backwards', 'cheetah-run-front', 'cheetah-run-back',
-		'cheetah-jump', 'hopper-hop-backwards', 'reacher-three-easy', 'reacher-three-hard', 'cup-spin',
-		'pendulum-spin',
-	],
-	'mt80': [
-		# 19 original dmcontrol tasks
-		'walker-stand', 'walker-walk', 'walker-run', 'cheetah-run', 'reacher-easy',
-	    'reacher-hard', 'acrobot-swingup', 'pendulum-swingup', 'cartpole-balance', 'cartpole-balance-sparse',
-		'cartpole-swingup', 'cartpole-swingup-sparse', 'cup-catch', 'finger-spin', 'finger-turn-easy',
-		'finger-turn-hard', 'fish-swim', 'hopper-stand', 'hopper-hop',
-		# 11 custom dmcontrol tasks
-		'walker-walk-backwards', 'walker-run-backwards', 'cheetah-run-backwards', 'cheetah-run-front', 'cheetah-run-back',
-		'cheetah-jump', 'hopper-hop-backwards', 'reacher-three-easy', 'reacher-three-hard', 'cup-spin',
-		'pendulum-spin',
+	'mt50': [
 		# meta-world mt50
 		'mw-assembly', 'mw-basketball', 'mw-button-press-topdown', 'mw-button-press-topdown-wall', 'mw-button-press',
 		'mw-button-press-wall', 'mw-coffee-button', 'mw-coffee-pull', 'mw-coffee-push', 'mw-dial-turn',
